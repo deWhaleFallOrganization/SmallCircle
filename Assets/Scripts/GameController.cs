1ba987039fe4767cfb1ae64708aac094ce9b2d8a
deWhaleFall <1286942798@qq.com>
2022-10-26 23:15:28 +0800

    泻药，640个球，已经卡死，项目停更（）----------什么光速放弃（）

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 internal class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Tooltip("小球预制件")]
+    public SmallCircleController smallCircleController;
 
-    // Update is called once per frame
-    void Update()
+    [Tooltip("参演的小球颜色")]
+    public Color[] colors;
+
+    [Tooltip("每个小球最高多少个")]
+    public int maxColors;
+
+    [Tooltip("出生点")]
+    public Transform birthPoint;
+
+    [Tooltip("用来存所有小球引用的东西")]
+    public SmallCircleController[][] smallCircleControllers;
+    [Tooltip("用来统计每个颜色的小球的数量")]
+    public int currSmlCleCount = 1;
+    private void Start()
     {
-        
+        smallCircleControllers = new SmallCircleController[colors.Length][];
+        for (int i = 0; i < smallCircleControllers.Length; i++)
+        {
+            smallCircleControllers[i] = new SmallCircleController[maxColors];
+        }
+        for (int i = 0; i < colors.Length; i++)
+        {
+            var instNewSmallCircle = Instantiate(smallCircleController, birthPoint.position, birthPoint.rotation);
+            instNewSmallCircle.spriteRenderer.color = colors[i];
+            smallCircleControllers[i][0] = instNewSmallCircle;
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            for (int i = 0; i < smallCircleControllers.Length; i++)
+            {
+                for (int j = 0; j < currSmlCleCount; j++)
+                {
+                    var instNewSmallCircle = Instantiate(smallCircleController, birthPoint.position, birthPoint.rotation);
+                    instNewSmallCircle.spriteRenderer.color = colors[i];
+                    smallCircleControllers[i][j + currSmlCleCount] = instNewSmallCircle;
+                }
+            }
+            currSmlCleCount *= 2;
+        }
     }
 }
---
+++ Assets/Scripts/SmallCircleController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+internal class SmallCircleController : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+
+}
